caf46cadc7a6234bc15b5bcf2f02d256a4eda394
Majuth <majuth101@hotmail.com>
2021-05-01 16:31:21 -0400

    Update inputs

--- src/mainpage.js
+++ src/mainpage.js
@@ -6,15 +6,50 @@ class mainPage extends React.Component {
         <div id="mainPage">
             <div class="container">
                 <div class="row">
+                    <h2>RUPrint</h2>
+                </div>
+                <div class="row">
                     <div class="col-4">
-                    One of three columns
+                        <h6>Program</h6>
+                        <form>
+                        <select id="cars" name="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="fiat">Fiat</option>
+                            <option value="audi">Audi</option>
+                        </select>
+                        <input type="submit" />
+                        </form>
                     </div>
                     <div class="col-4">
-                    One of three columns
+                        <h6>Year Range</h6>
+                        <form>
+                            <input type="date"></input>
+                        </form>
                     </div>
                     <div class="col-4">
-                    3 of three columns
+                        <h6>Co-op</h6>
                     </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                    One column
+                    </div>
+                    <div class="col-2">
+                    Two column
+                    </div>
+                    <div class="col-2">
+                    Three column
+                    </div>
+                    <div class="col-2">
+                    Fourth column
+                    </div>
+                    <div class="col-2">
+                    Fifth column
+                    </div>
+                </div>
+                <div class="row">
+                    
                 </div>
             </div>
         </div>
