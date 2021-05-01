02c1aaf6251efebdce518d8f3cfd208a47a7476a
Majuth <majuth101@hotmail.com>
2021-05-01 16:57:57 -0400

    Started course page layout aswell

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import MainPage from './mainPage.js';
+import CoursePage from './coursePage.js';
 
 
 function App() {
   return (
     <div className="App">
-        <MainPage />
+        <div class="container">
+          <div class="row">
+            <h2>RUPrint</h2>
+          </div>
+          <div class="row">
+            <CoursePage />
+          </div>
+        </div>
     </div>
   );
 }
--- src/mainpage.js
+++ src/mainpage.js
@@ -6,9 +6,6 @@ class mainPage extends React.Component {
         <div id="mainPage">
             <div class="container">
                 <div class="row">
-                    <h2>RUPrint</h2>
-                </div>
-                <div class="row">
                     <div class="col-4">
                         <h6>Program</h6>
                         <form>
@@ -24,11 +21,18 @@ class mainPage extends React.Component {
                     <div class="col-4">
                         <h6>Year Range</h6>
                         <form>
+                            Start Year:
+                            <input type="date"></input>
+                            End Year:
                             <input type="date"></input>
                         </form>
                     </div>
                     <div class="col-4">
                         <h6>Co-op</h6>
+                        <form>
+                            In Co-op
+                            <input type="checkbox"></input>
+                        </form>
                     </div>
                 </div>
                 <div class="row">
@@ -49,7 +53,10 @@ class mainPage extends React.Component {
                     </div>
                 </div>
                 <div class="row">
-                    
+                    <h6>Required Credits 15/30</h6>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style={{width: '50%'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
                 </div>
             </div>
         </div>
