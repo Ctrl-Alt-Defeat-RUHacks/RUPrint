f62a398c9031fea8da468d942913ed2259b13c0d
Majuth <majuth101@hotmail.com>
2021-05-01 15:41:39 -0400

    Update starting layout

--- src/mainpage.js
+++ src/mainpage.js
@@ -4,8 +4,16 @@ class mainPage extends React.Component {
     render() {
       return (
         <div id="mainPage">
-            <p>Content goes here</p>
+            <p>Content goes here.
+            </p>
+            <table>
+                <tr>
+                    <td>First content here</td>
+                </tr>
+            </table>
         </div>
       );
     }
-  }
+}
+
+export default mainPage;
